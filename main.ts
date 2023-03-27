197d0150836c74447935f9179df79c2621efe5dd
lalahg06 <129099832+lalahg06@users.noreply.github.com>
2023-03-27 09:58:33 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showString("lalah")
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
